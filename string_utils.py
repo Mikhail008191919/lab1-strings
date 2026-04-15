b7942ebcbbfc4b62f9b28d15d22778931e6f774e
Mikhail Kalinovich <kalinovich2007@gmail.com>
2026-04-15 10:44:45 +0700

    Добавлена функция смены регистра (вариант 2b)

---
+++ string_utils.py
@@ -0,0 +1,10 @@
+def to_upper(s):
+    return s.upper()
+
+def to_lower(s):
+    return s.lower()
+
+if __name__ == "__main__":
+    text = input("Введите строку: ")
+    print("Верхний регистр:", to_upper(text))
+    print("Нижний регистр:", to_lower(text))
