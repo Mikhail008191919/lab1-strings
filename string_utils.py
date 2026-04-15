ecad9a76b2fe33f9bf6f2ebe5df001bdb1cf4d4b
Kalinovich Mikhail <kalinovich2007@gmail.com>
2026-04-15 11:08:49 +0700

    Добавлен комментарий

--- string_utils.py
+++ string_utils.py
@@ -8,3 +8,4 @@ if __name__ == "__main__":
     text = input("Введите строку: ")
     print("Верхний регистр:", to_upper(text))
     print("Нижний регистр:", to_lower(text))
+# Дополнительное изменение
